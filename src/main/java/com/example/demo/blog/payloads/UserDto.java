a19326eea3a8c7e866d89150364dc7ca14409479
sagar <sagaraigandhi@gmail.com>
2022-08-28 17:35:44 +0900

    Intial Project

---
+++ src/main/java/com/example/demo/blog/payloads/UserDto.java
@@ -0,0 +1,64 @@
+package com.example.demo.blog.payloads;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+public class UserDto {
+
+	private Integer id ;
+	private String name;
+	private String email;
+	private String password;
+	private String about ;
+	
+	public UserDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAbout() {
+		return about;
+	}
+
+	public void setAbout(String about) {
+		this.about = about;
+	}
+	
+	
+
+	
+}
